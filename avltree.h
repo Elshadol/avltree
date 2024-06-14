31c20ef27a42ee5c46fdf24f07a0836bbd99563f
Kim Kowk <31947243+Elshadol@users.noreply.github.com>
2024-06-14 22:35:00 +0800

    Update avltree.h

--- avltree.h
+++ avltree.h
@@ -16,29 +16,6 @@ struct avl_root {
     struct avl_node *avl_node;
 };
 
-#define avl_parent(n) \
-    ((struct avl_node *)((n)->__avl_parent_bf & ~3lu))
-#define avl_bf(n) \
-    ((unsigned long)((n)->__avl_parent_bf & 3lu))
-
-static inline void avl_set_parent(struct avl_node *n,
-                                  const struct avl_node *p)
-{
-    n->__avl_parent_bf =
-        (n->__avl_parent_bf & 3lu) | (unsigned long)(p);
-}
-
-static inline void avl_set_bf(struct avl_node *n,
-                              unsigned long b)
-{
-    n->__avl_parent_bf =
-        (n->__avl_parent_bf & ~3lu) | (unsigned long)(b);
-}
-
-#define avl_left_heavy(n) (avl_bf(n) == AVL_LEFT_HEAVY)
-#define avl_right_heavy(n) (avl_bf(n) == AVL_RIGHT_HEAVY)
-#define avl_balanced(n) (avl_bf(n) == AVL_BALANCED)
-
 #define AVL_ROOT (struct avl_root) { NULL, }
 
 #define	avl_entry(ptr, type, member) __extension__ ({ \
@@ -49,9 +26,10 @@ static inline void avl_set_bf(struct avl_node *n,
         const typeof(ptr) ___ptr = (ptr); \
         ___ptr ? avl_entry(___ptr, type, member) : NULL; })
 
-#define AVL_EMPTY_ROOT(root) ((root)->avl_node == NULL)
-#define AVL_EMPTY_NODE(node) ((avl_parent((node)) == (node))
-#define AVL_CLEAR_NODE(node) (avl_set_parent((node), (node)))
+#define AVL_EMPTY_NODE(node) \
+    ((node)->__avl_parent_bf == (unsigned long)(node))
+#define AVL_CLEAR_NODE(node) \
+    ((node)->__avl_parent_bf = (unsigned long)(node))
 
 void avl_insert_fixup(struct avl_node *inserted, struct avl_root *root);
 void avl_erase(struct avl_node *delete, struct avl_root *root);
@@ -65,7 +43,7 @@ void avl_replace_node(struct avl_node *victim, struct avl_node *newnode,
                       struct avl_root *root);
 
 static inline void avl_link_node(struct avl_node *node,
-                                 const struct avl_node *parent,
+                                 struct avl_node *parent,
                                  struct avl_node **avl_link)
 {
     node->__avl_parent_bf = (unsigned long)parent;
