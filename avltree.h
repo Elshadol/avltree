1c0f09fbbbd17b08fd9994ea149318f679db2992
Kim Kowk <31947243+Elshadol@users.noreply.github.com>
2024-07-13 15:44:12 +0800

    Update avltree.h

--- avltree.h
+++ avltree.h
@@ -1,37 +1,40 @@
-#ifndef	AVLTREE_INCLUDED
-#define	AVLTREE_INCLUDED
+#ifndef AVL_H_
+#define AVL_H_
 
 #include <stddef.h>
 
 struct avl_node {
-    unsigned long __avl_parent_bf;
-#define AVL_BALANCED    0lu        // 00
-#define AVL_RIGHT_HEAVY 1lu        // 01
-#define AVL_LEFT_HEAVY  2lu        // 10
-    struct avl_node *avl_right;
-    struct avl_node *avl_left;
+  unsigned long __avl_parent_bf;
+#define AVL_BALANCED 0lu    // 00
+#define AVL_RIGHT_HEAVY 1lu // 01
+#define AVL_LEFT_HEAVY 2lu  // 10
+  struct avl_node *avl_right;
+  struct avl_node *avl_left;
 } __attribute__((aligned(sizeof(long))));
 
 struct avl_root {
-    struct avl_node *avl_node;
+  struct avl_node *avl_node;
 };
 
-#define AVL_ROOT (struct avl_root) { NULL, }
+#define AVL_ROOT                                                               \
+  (struct avl_root) { NULL, }
 
-#define	avl_entry(ptr, type, member) __extension__ ({ \
-	const __typeof__(((type *)0)->member) *__pmember = (ptr); \
-	(type *)((char *)__pmember - offsetof(type, member)); })
+#define avl_entry(ptr, type, member)                                           \
+  __extension__({                                                              \
+    const __typeof__(((type *)0)->member) *__pmember = (ptr);                  \
+    (type *)((char *)__pmember - offsetof(type, member));                      \
+  })
 
-#define avl_entry_safe(ptr, type, member) __extension__ ({ \
-        const typeof(ptr) ___ptr = (ptr); \
-        ___ptr ? avl_entry(___ptr, type, member) : NULL; })
+#define avl_entry_safe(ptr, type, member)                                      \
+  __extension__({                                                              \
+    const typeof(ptr) ___ptr = (ptr);                                          \
+    ___ptr ? avl_entry(___ptr, type, member) : NULL;                           \
+  })
 
-#define AVL_EMPTY_NODE(node) \
-    ((node)->__avl_parent_bf == (unsigned long)(node))
-#define AVL_CLEAR_NODE(node) \
-    ((node)->__avl_parent_bf = (unsigned long)(node))
+#define AVL_EMPTY_NODE(node) ((node)->__avl_parent_bf == (unsigned long)(node))
+#define AVL_CLEAR_NODE(node) ((node)->__avl_parent_bf = (unsigned long)(node))
 
-void avl_insert_fixup(struct avl_node *inserted, struct avl_root *root);
+void avl_insert_rebalance(struct avl_node *inserted, struct avl_root *root);
 void avl_erase(struct avl_node *delete, struct avl_root *root);
 
 struct avl_node *avl_next(const struct avl_node *);
@@ -42,20 +45,18 @@ struct avl_node *avl_last(const struct avl_root *);
 void avl_replace_node(struct avl_node *victim, struct avl_node *newnode,
                       struct avl_root *root);
 
-static inline void avl_link_node(struct avl_node *node,
-                                 struct avl_node *parent,
-                                 struct avl_node **avl_link)
-{
-    node->__avl_parent_bf = (unsigned long)parent;
-    node->avl_left = NULL;
-    node->avl_right = NULL;
-    *avl_link = node;
+static inline void avl_link_node(struct avl_node *node, struct avl_node *parent,
+                                 struct avl_node **avl_link) {
+  node->__avl_parent_bf = (unsigned long)parent;
+  node->avl_left = NULL;
+  node->avl_right = NULL;
+  *avl_link = node;
 }
 
-static inline void avl_erase_init(struct avl_node *node, struct avl_root *root)
-{
-    avl_erase(node, root);
-    AVL_CLEAR_NODE(node);
+static inline void avl_erase_init(struct avl_node *node,
+                                  struct avl_root *root) {
+  avl_erase(node, root);
+  AVL_CLEAR_NODE(node);
 }
 
-#endif	/* avlTREE_INCLUDED */
+#endif
